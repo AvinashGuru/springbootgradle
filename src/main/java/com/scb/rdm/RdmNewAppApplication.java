c9a5f53751ee78bd5e6e63ef6fc7ad449934bdab
AvinashGuru <31862054+AvinashGuru@users.noreply.github.com>
2023-11-23 15:09:28 +0530

    Add files via upload

---
+++ src/main/java/com/scb/rdm/RdmNewAppApplication.java
@@ -0,0 +1,13 @@
+package com.scb.rdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdmNewAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdmNewAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scb/rdm/RdmNewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.rdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdmNewAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
